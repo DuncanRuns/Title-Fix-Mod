f4548ca211563337bc60ee73d4271d3c8271b1f3
DuncanRL <59705125+DuncanRuns@users.noreply.github.com>
2022-05-02 05:02:55 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Title-Fix-Mod
+ When exiting a world or server, the in game title overlay will be cleared to prevent transfering to the next world.
---
+++ src/main/java/me/duncanruns/titlefix/mixin/MinecraftClientMixin.java
@@ -0,0 +1,23 @@
+package me.duncanruns.titlefix.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.gui.screen.Screen;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MinecraftClient.class)
+public abstract class MinecraftClientMixin {
+    @Shadow
+    @Final
+    public InGameHud inGameHud;
+
+    @Inject(method = "disconnect(Lnet/minecraft/client/gui/screen/Screen;)V", at = @At("HEAD"))
+    private void clearTitleMixin(Screen screen, CallbackInfo info) {
+        inGameHud.setTitles(null, null, 10, 70, 20); // 10, 70, 20 is the default title timings
+    }
+}
